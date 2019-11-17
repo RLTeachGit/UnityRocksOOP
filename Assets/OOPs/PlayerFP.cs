e0ae6292317d05480ef42154f0ad1abf214f53aa
rleinfellner <r.leinfellner@brighton.ac.uk>
2019-11-17 15:05:50 +0000

    First Upload

---
+++ Assets/OOPs/PlayerFP.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFP : FakePhysics
+{
+    protected override void DoMove() {
+        Vector2 tMoveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); //Get User input
+
+        transform.Rotate(0, 0, -tMoveInput.x * MaxRotation * Time.deltaTime);
+
+        //Rotate ship on player command, note axis reversed
+        Vector2 tForce = transform.up;  //Up vector is now direction we are pointing in
+        tForce *= MaxSpeed * tMoveInput.y; //Apply User input & MaxSpeed
+        mVelocity += tForce;    //Calculate new velocity, scale for time
+        base.DoMove(); //Call base class to update position
+    }
+
+    protected override void CollidedWith(FakePhysics vOtherFF) {
+        Debug.LogFormat("Player hit by {0}", vOtherFF.name); //Player specifc code
+        //We do not call parent as we will handle
+    }
+}
---
+++ Assets/OOPs/RockBigFP.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockBigFP : FakePhysics {
+    // Start is called before the first frame update
+
+    float mRotation;
+
+    protected override void Start() {
+        mVelocity = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized*MaxSpeed; //Initial velocity
+        mRotation = Random.Range(-4.0f, 4.0f);
+        base.Start(); //Now call parent
+    }
+
+    //Rock's own movement
+    protected override void DoMove() {
+        transform.Rotate(0, 0, -mRotation * MaxRotation * Time.deltaTime);
+        base.DoMove();
+    }
+
+    protected override void CollidedWith(FakePhysics vOtherFF) {
+        Debug.LogFormat("RockBigFP hit by {0}", vOtherFF.name); //RockBigFP specifc code
+        //We do not call parent as we will handle
+    }
+
+}
